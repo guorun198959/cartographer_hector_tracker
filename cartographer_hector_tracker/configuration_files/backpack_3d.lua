1cadcf450c761baf75668346318de1e330d27dfb
Kevin Daun <daun@sim.tu-darmstadt.de>
2018-01-15 11:08:11 +0100

    Added launch files and configs for head evaluation 01_15_18

--- cartographer_hector_tracker/configuration_files/backpack_3d.lua
+++ cartographer_hector_tracker/configuration_files/backpack_3d.lua
@@ -20,35 +20,34 @@ options = {
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
   tracking_frame = "base_link",
-  published_frame = "odom",
+  published_frame = "base_link",
   odom_frame = "odom",
-  provide_odom_frame = false,
-  use_odometry = true,
-  use_laser_scan = false,
-  use_multi_echo_laser_scan = false,
-  num_point_clouds = 1,
+  provide_odom_frame = true,
+  use_odometry = false,
+  use_nav_sat = false,
+  num_laser_scans = 0,
+  num_multi_echo_laser_scans = 0,
+  num_subdivisions_per_laser_scan = 1,
+  num_point_clouds = 2,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
+  rangefinder_sampling_ratio = 1.,
+  odometry_sampling_ratio = 1.,
+  fixed_frame_pose_sampling_ratio = 1.,
+  imu_sampling_ratio = 1.,
 }
 
-TRAJECTORY_BUILDER_3D.scans_per_accumulation = 50
-TRAJECTORY_BUILDER_3D.min_range = 0.5
-TRAJECTORY_BUILDER_3D.max_range = 20.
-TRAJECTORY_BUILDER_3D.submaps.num_range_data = 40.
-TRAJECTORY_BUILDER_3D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 160
 
 MAP_BUILDER.use_trajectory_builder_3d = true
 MAP_BUILDER.num_background_threads = 7
-SPARSE_POSE_GRAPH.optimization_problem.huber_scale = 5e2
-SPARSE_POSE_GRAPH.optimize_every_n_scans = 40
-SPARSE_POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
-SPARSE_POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
--- Reuse the coarser 3D voxel filter to speed up the computation of loop closure
--- constraints.
-SPARSE_POSE_GRAPH.constraint_builder.adaptive_voxel_filter = TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter
-SPARSE_POSE_GRAPH.constraint_builder.min_score = 0.62
-SPARSE_POSE_GRAPH.constraint_builder.log_matches = true
+POSE_GRAPH.optimization_problem.huber_scale = 5e2
+POSE_GRAPH.optimize_every_n_nodes = 320
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
+POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
+POSE_GRAPH.constraint_builder.min_score = 0.62
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66
 
 return options
